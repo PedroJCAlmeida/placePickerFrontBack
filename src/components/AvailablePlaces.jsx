49643473eaa5ccdabcef96bc05e412317e92064f
pedro.almeida <pedro.almeida@rtcom.pt>
2025-02-17 15:43:20 +0000

    first commit

---
+++ src/components/AvailablePlaces.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import Places from './Places.jsx';
+import { useEffect } from 'react';
+
+export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+  
+  useEffect(() => {
+    fetch("http://localhost:3000/places")
+      .then((respose) =>{
+      return respose.json();
+    })
+    .then((resData) => {
+      setAvailablePlaces(resData.places);
+    }); 
+  }, []);
+
+  return (  
+    <Places
+      title="Available Places"
+      places={availablePlaces}
+      fallbackText="No places available."
+      onSelectPlace={onSelectPlace}
+    />
+  );
+}
