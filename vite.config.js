62375b21040dbc252ca072edff0c6b1a1a7ff1f1
pedro.almeida <pedro.almeida@rtcom.pt>
2025-02-18 12:36:43 +0000

    Update Page

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://pedrojcalmeida.github.io/portfolio-place-picker",
+  base: "https://pedrojcalmeida.github.io/portfolio-place-picker/",
   plugins: [react()],
 })
